a55a3b80854212fe75f2bb35b359fa5467ab38fa
CleoCleoCrv <c_corvee@stu-psbedu.paris>
2021-06-19 13:00:29 +0200

    MAJ avec page à propos
    Page about pour la page à propos.

---
+++ about.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html style="font-size: 16px">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="page_type" content="np-template-header-footer-from-plugin" />
+  <title>A propos du projet</title>
+  <link rel="stylesheet" href="./styles/nicepage.css" media="screen" />
+  <link rel="stylesheet" href="./styles/Trouver-une-banque.css" media="screen" />
+  <link rel="stylesheet" href="./styles/A-propos-du-projet.css" media="screen" />
+  <script class="u-script" type="text/javascript" src="./script/jquery-1.9.1.min.js" defer=""></script>
+  <script class="u-script" type="text/javascript" src="./script/nicepage.js" defer=""></script>
+  <meta name="generator" content="Nicepage 3.17.5, nicepage.com" />
+  <link id="u-theme-google-font" rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" />
+
+  <script type="application/ld+json">
+      {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "name": "",
+        "logo": "images/200930789_200446098612759_2343269356248476456_n.png"
+      }
+    </script>
+  <meta property="og:title" content="A propos du projet" />
+  <meta property="og:type" content="website" />
+  <meta name="theme-color" content="#f15048" />
+</head>
+
+<body class="u-body">
+    
+  <?php require('./composants/navbar.php') ?>
+  
+  <section class="u-clearfix u-palette-4-dark-2 u-section-1" id="sec-c67a">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-align-center u-custom-font u-font-oswald u-text u-text-default u-text-3">A propos du projet</h1>
+      </div></section>
+    <section class="u-clearfix u-white u-section-2" id="sec-3d87">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <h3>Pourquoi MYMG ?</h3>
+		<div class="video">
+	  <iframe width="560" height="315" src="https://www.youtube.com/embed/9wbt-cwq1BE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	  </div>
+        <p class="u-text u-font-courier-new u-text-2">Contrairement à ce que beaucoup de gens pensent, <b>l’argent épargné sur votre livret A ne dort pas dans un coffre</b>. Bien au contraire, 
+		il circule et permet à votre banque de financer des industries sur lesquelles vous n’avez pas la main. Usines à charbon, extraction pétrolière... à cause de ces financements, nous participons tous indirectement à la crise climatique.
+Pour vous permettre de reprendre le pouvoir sur votre argent, le projet MYMG est né. <br/>
+Make your money green, c’est <b>un comparateur basé sur les objectifs du développement durable</b>. Il vous permet de confronter toutes les banques françaises  pour que vous puissiez choisir celle qui vous correspondra le mieux en termes de : 
+
+				<ul>
+										<li>Transparence</li>
+										<li>Valeurs</li>
+										<li>Engagements</li>
+										<li>Utilisation</li>
+										<li>Rémunération</li>
+										
+									</ul>
+<span><b>Notre service est 100% gratuit</b>, et notre site est conçu avec un faible impact carbone. <br/></span>
+<span>Pas encore assez convaincu pour quitter votre banque actuelle pour une banque plus green ? Faites le test et <a href="https://www.oxfamfrance.org/climat-et-energie/calculez-lempreinte-carbone-de-votre-compte-bancaire/">calculez l’empreinte carbone</a> de vos comptes bancaires.</span>
+</p>
+
+      </div>
+    </section>
+
+<section>
+<h3>La dream team MYMG</h3>
+<article>
+<div class="articleatelier u-clearfix u-palette-4-light-3 u-section-3">
+<img src="images/Helo.jpg" alt="photo helo" width="300px" class="image-left">
+<div class="paragraphe">
+<h4><b>Héloïse, Dev back-end</b></h4>
+<p>Dévouée depuis toujours à l’écologie, je suis fière d’avoir rejoint l’équipe pour mettre mes compétences 
+techniques au service de la vision MYMG en matière d’environnement et de solidarité.</p>
+</div></div>
+</article>
+<article>
+<div class="articleatelier">
+<img src="images/morgane.jpg" alt="photo morgane" width="300px" class="image-left">
+<div class="paragraphe">
+<h4><b>Morgane, DA</b></h4>
+<p>Sensible et consciente des crises environnementales qui nous touchent, 
+je fais partie de cette génération qui a besoin de créer des concepts respectueux de notre planète. 
+L’idée de MYMG permet à toutes les personnes d’investir leurs épargnes dans des projets fidèles à leurs valeurs et principes. 
+Pour des projets durables et éco-responsables. Cette idée nous est venue après la conclusion que nos petits gestes du quotidien sont entachés par les investissements que les banques font grâce à nos épargnes. 
+Après cette prise de conscience, nous avons décidé que c’est à nous, la  nouvelle génération de décider quel projet devait être investi ou non. Des projets durables et respectueux de l’environnement. Pour un avenir meilleur, make your money green.</p>
+</div>
+</div>
+</article>
+<article>
+<div class="articleatelier u-clearfix u-palette-4-light-3 u-section-3">
+<img src="images/fx.jpg" alt="photo fx" width="300px" class="image-left" align="left">
+<div class="paragraphe">
+<h4><b>François-Xavier, Community Manager</b></h4>
+<p>Soucieux de l’environnement et des différents problèmes sociaux qui affectent notre société, j’ai choisi de rejoindre l’entreprise MYMG pour participer à un projet innovant autour de personnes motivantes.<br /> 
+Développer et communiquer sur un produit gratuit et responsable est pour moi une réelle plus-value qui correspond à mes valeurs. 
+</p>
+</div></div>
+</article>
+<article>
+<div class="articleatelier">
+<img src="images/cleo.jpg" alt="photo cleo" width="300px" class="image-left" >
+<div class="paragraphe">
+<h4><b>Cléo, Dev Front-end</b></h4>
+<p>Au cours de mes différents voyage en backpack, j'ai pu contaster de mes propre yeux l'impact du réchauffement climatique sur notre planète. 
+La Patagonie, un territoire que j'affectionne tout particulièrement, est sujette à toutes les envies des grands groupes financiers d'inverstir dans l'extraction du gaz des shiste. <br/> Pour inverser cette tendence et agir à mon échelle, 
+j'ai choisi de changer de banque pour une autre très transarente sur ses actions de financements, beaucoup plus durables. Mais j'ai eu beaucoup de mal à choisir car aucun outil ne me permettait alors de comparer 
+les meilleurs banques en terme de responsabilité sociale et environnementales. MYMG m'est apparue comme une évidence.</p>
+</div>
+</div>
+</article>
+<article>
+<div class="articleatelier u-clearfix u-palette-4-light-3 u-section-3">
+<img src="images/amine.jpg" alt="photo amine" width="300px" class="image-left">
+<div class="paragraphe">
+<h4><b>Amine, UX designer</b></h4>
+<p>Avec une forte volonté d’évoluer dans des environnements de travail portant des valeurs humaines et environnementales fortes, j’ai souhaité inscrire mon projet professionnel avec MYMG.<br/>
+Ma mission :  Mieux comprendre le besoin, l’expérience et les interactions des utilisateurs de la plateforme MYMG afin de mieux répondre à leur besoin.</p>
+</div></div>
+</article>
+<article>
+<div class="articleatelier">
+<img src="images/yoan.jpg" alt="photo yoan" width="300px" class="image-left" >
+<div class="paragraphe">
+<h4><b>Yoan, Chef de Projet</b></h4>
+<p>L’écologie et la protection de l’environnement ayant toujours été au centre de mes intérêts ainsi que les problèmes de notre société, je suis très heureux de pouvoir participer à un projet, 
+une vision telle que celle de  MYMG, accompagné par une équipe motivante et qualifiée.</p>
+</div>
+</div>
+</article>
+<article>
+<div class="articleatelier u-clearfix u-palette-4-light-3 u-section-3">
+<img src="images/sylvia.jpg" alt="photo sylvia" width="300px" class="image-left">
+<div class="paragraphe">
+<h4><b>Sylvia, Marketing Manager</b></h4>
+<p>Avec une forte volonté d’évoluer dans des environnements de travail portant des valeurs humaines et environnementales fortes, j’ai souhaité inscrire mon projet professionnel avec MYMG.<br/>
+Ma mission :  Mieux comprendre le besoin, l’expérience et les interactions des utilisateurs de la plateforme MYMG afin de mieux répondre à leur besoin.</p>
+</div></div>
+</article>
+
+
+
+
+
+  
+</section>
+  
+  
+  
+  
+  <?php require('./composants/footer.php') ?>
+</body>
+
+</html>
